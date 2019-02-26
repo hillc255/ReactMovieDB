be04f56ed55c2f083fc2b1eaf554cd76817b2636
Claudia Hill <hillc255@yahoo.com>
2019-02-26 04:21:06 -0500

    added route to folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import NotFound from '../elements/NotFound/NotFound';
 
 const App = () => {
    return (
-       <BrowserRouter>
+    <BrowserRouter basename="/ReactMovieDB">
        <React.Fragment>
            <Header />
            <Switch>
