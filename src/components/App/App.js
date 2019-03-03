11882b3cf3bba1014945f2e46c686af2a20c157f
Claudia Hill <hillc255@yahoo.com>
2019-03-03 05:17:37 -0500

    remove BrowserRouter folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import NotFound from '../elements/NotFound/NotFound';
 
 const App = () => {
    return (
-    <BrowserRouter basename="/ReactMovieDB">
+    <BrowserRouter>
        <React.Fragment>
            <Header />
            <Switch>
